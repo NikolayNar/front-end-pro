d7522f010f5ec13a425e2c27af9d0044074929f5
Nikolas <n.narivskiy@gmail.com>
2021-08-11 21:55:57 +0300

    lesson 4

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,14 @@
-alert('Hello! Let`s get to know each other better. Are you ready?')
+alert('Let`s create your color');
 
-var numberAge = +prompt('How old are you?');
+var numberRedColor = +prompt('How much RED color?');
+var numberGreenColor = +prompt('How much GREEN color?');
+var numberBlueColor = +prompt('How much BLUE color?');
 
-if (numberAge == '' || null || undefined) {
-    alert('Error!!! It`s not an age!');
-    alert('Lets try again. Refresh me after click "OK"');
-} else {
-    var smoke = confirm('Do you smoke? If "YES" click "OK", if "NO" - "CANCEL"');
+function rgb(red, green, blue){
+    var color = red + ',' + green + ',' + blue;
+    return color;
 };
 
-console.log('Age --> ', numberAge);
-console.log('Smoking --> ', smoke);
+var result = rgb(numberRedColor, numberGreenColor, numberBlueColor);
 
-
-if (numberAge < 18 && smoke === false){
-    alert('Great! You`re too young!');
-} else if (numberAge < 18 && smoke === true){
-    alert('Oh my God! I`ll told your mother!');
-} else if (numberAge >= 18 && smoke === false){
-    alert('Amazing! Keep it going!');
-}else if (numberAge >= 18 && smoke === true){
-    alert('Congrats! You`ll die so soon!');
-} else{
-    alert('Don`t try to cheat Me! I`m Skynet!');
-}
+console.log('rbg('+result+')');
