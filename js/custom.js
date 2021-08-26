abbe01e1004d61478972cfd38158886cb8c32e0b
Nikolas <n.narivskiy@gmail.com>
2021-08-26 17:02:15 +0300

    homework9

--- js/custom.js
+++ js/custom.js
@@ -1,51 +1,30 @@
-// №1
-var arr1 = ['a', 'b', 'c'];
-arr1.push(1, 2, 3);
-console.log('arr1 -->', arr1);
-
-// №2
-var arr2 = [1, 2, 3];
-arr2.reverse();
-console.log('arr2 -->', arr2);
-
-// №3
-var arr3 = [1, 2, 3];
-arr3.unshift(4, 5, 6);
-console.log('arr3 -->', arr3);
-
-// №4
-var arr4 = [1, 2, 3, 4, 5];
-var newArr4 = arr4.slice(0, 3)
-console.log('arr4 -->', newArr4);
-
-// №5
-var arr5 = ['js', 'css', 'jq'];
-console.log('arr5 -->', arr5[0]);
-// если прям вывести на экран, то:
-alert(arr5[0]);
-// ну или:
-document.write(arr5[0]);
-
-// №6 я к сожалению не нашел как из 'a' и 'b' в масивах сделать ключи для обЪекта. Можешь написать как надо было сделать.
-var fromPairs = (array) => {
-    var array = new Object();
-    var array = {
-        a: data[0][1],
-        b: data[1][1]
+const obj = {
+    prop: '42',
+    prop2: [8, 9, 10, {
+      beautifulPropertyName: 88,
+      'property with spaces': {
+        a: {
+          b: '',
+          c: {
+            someProperty: [{
+              'prop name': 'I am a smart programmer',
+            }],
+          },
+        },
+      },
+    }],
+    prop3: function() {
+      return {
+        baz: 'Hello',
+        bar: {
+          anotherBeautifulProp: [8, {
+            target: 'It was simple!',
+          }],
+        },
+      };
     }
-    return array;
-}
+  };
 
-const data = [['a', 1], ['b', 2]];
+console.log('obj', obj.prop2[3]['property with spaces'].a.c.someProperty[0]['prop name']);
 
-console.log(fromPairs(data));
-
-// №6_1 Но зато я нашел метод Object.fromEntries(), которые собственно это и делает, но не нашел его действие под капотом.
-var fromPairs = (array) => {
-    array = Object.fromEntries(datta);
-    return array;
-}
-
-const datta = [['a', 1], ['b', 2]];
-
-console.log(fromPairs(datta))
+console.log('obj', obj.prop3().bar.anotherBeautifulProp[1].target);
