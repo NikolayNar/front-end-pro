407fb719b306a52b5dea59c44ab5cdd2b7bf0af3
Nikolas <n.narivskiy@gmail.com>
2021-09-13 17:08:13 +0300

    homework12

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,22 @@
-// With For
+function num(a) {
+  let num = a;
 
-function max(arr) {
-  let bigNumber = null;
-
-  for (let i of arr) {
-    if (i > bigNumber) {
-      bigNumber = i;
-    }
-  } 
-  return bigNumber
-  
+  return {
+    add: function(b) {
+      if (isNaN(b)) {
+        return NaN;
+      }
+      
+      return num += b;
+    },
+    sub: function(b) {
+      return num -= b;
+    },
+    set: function(b) {
+      return num = b
+    },
+    get: () => num
+  }
 }
 
-console.log('Solve with "For" loop:')
-console.log(max([8]), 'one element test, must return 8');
-console.log(max([8, 17]), '2 elements test, must return 17'); 
-console.log(max([1, 8, 37, 5, 17]), '5 elements test, must return 37');
-
-// With Recursion
-
-function max(arr) {
-  if (arr.length <= 1 ) {
-    return arr[0];
-  }
-
-  let lastEl = arr[arr.length - 1];
-
-  if (lastEl > arr[0]) {
-    arr[0] = lastEl
-  }
-  arr.pop()
-  
-  return max(arr)
-}
-
-console.log('Solve with "Recursion":')
-console.log(max([8]), 'one element test, must return 8');
-console.log(max([8, 17]), '2 elements test, must return 17');
-console.log(max([1, 8, 37, 5, 17]), '5 elements test, must return 37');
+const myNum = num(100);
